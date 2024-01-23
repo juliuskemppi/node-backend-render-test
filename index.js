62f96466bc4422d44adf7eeab0518837de5553e5
Julius Kemppi <julius.kemppi@gmail.com>
2024-01-23 15:17:39 +0200

    Updated port

--- index.js
+++ index.js
@@ -58,26 +58,26 @@ const generateId = () => {
   }
 
 app.post('/api/notes', (request, response) => {
-    const body = request.body
+      const body = request.body
 
-    if (!body.content) {
-      return response.status(400).json({ 
-        error: 'content missing' 
-      })
-    }
-  
-    const note = {
-      content: body.content,
-      important: body.important || false,
-      id: generateId(),
-    }
-  
-    notes = notes.concat(note)
-  
-    response.json(note)
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
   })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
